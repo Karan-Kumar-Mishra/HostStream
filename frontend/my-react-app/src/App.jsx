0f2cdf6bda64c888251d42f51f1014fc7dd90c3b
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2025-02-28 11:20:47 +0530

    create folder and add some config files

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import DashboardLayoutBasic from './Components/DashboardLayoutBasic'
+
+function App() {
+  
+
+  return (
+   <DashboardLayoutBasic/>
+  )
+}
+
+export default App
